ab55cb7137267d32c2972be7de26317a84872266
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2020-05-02 14:05:14 +0200

    enable CI testing for multi-threading

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
 using IntervalRootFinding2
 using Test
+using Base.Threads
 
-include("branch_and_bound.jl")
-include("search_interface.jl")
-include("roots.jl")
-include("test_smiley.jl")
+if nthreads() == 1
+    include("branch_and_bound.jl")
+    include("search_interface.jl")
+    include("roots.jl")
+    include("test_smiley.jl")
+elseif nthreads() > 1
+    include("parallel.jl")
+end
