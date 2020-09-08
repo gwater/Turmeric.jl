233db860ae43d3be18ba80ed1178237bc2e32705
Josua Grawitter <josua.grawitter@campus.tu-berlin.de>
2020-09-08 23:54:11 +0200

    update

--- bin/immutable_test.jl
+++ bin/immutable_test.jl
@@ -7,12 +7,13 @@ import Turmeric: GradientContractor, Newton, _isempty,
 
 import Base: push!, append!
 
-struct StoreFirst{T}
+struct StoreApplied{T, F}
     output::T
+    f::F
 end
 
-push!(d::StoreFirst, x) = StoreFirst(push!(d.output, first(x)))
-append!(d::StoreFirst, xs) = foreach(x -> push!(d, x), xs)
+push!(d::StoreApplied, x) = StoreApplied(push!(d.output, d.f(x)), d.f)
+append!(d::StoreApplied, xs) = foreach(x -> push!(d, x), xs)
 
 function sieve!(f, discarded)
     return function ff(data)
@@ -45,16 +46,21 @@ _intersect((region, contraction)::Tuple{T, T}) where T <: Number =
 _intersect((region, contraction)::Tuple{T, T}) where T <: AbstractArray =
     region .∩ contraction
 
-function _setup_search(with_contains_root, with_contraction_and_isinterior, with_too_small, root_regions, indeterminate_regions, tolerance)
+function setup_search(f, contractor, root_regions, indeterminate_regions, tolerance)
+    with_contains_root = _appended(strictly_contains_root(f))
+    with_contraction_and_isinterior =
+        _appended(strictly_interior) ∘ _appended(contractor)
+    with_region_too_small = _appended(too_small(tolerance) ∘ first)
+
     return function filter_contract_and_bisect(regions)
         remaining_region_tuples =
             regions |>
             _tmap(with_contains_root) |>
             _filter(last) |>
             _tmap(with_contraction_and_isinterior ∘ first) |>
-            sieve!(last, StoreFirst(StoreFirst(root_regions))) |>
-            _tmap(_appended(too_small(tolerance) ∘ first) ∘ first) |>
-            sieve!(last, StoreFirst(StoreFirst(indeterminate_regions))) |>
+            sieve!(last, StoreApplied(root_regions, first ∘ first)) |>
+            _tmap(with_region_too_small ∘ first) |>
+            sieve!(last, StoreApplied(indeterminate_regions, first ∘ first)) |>
             _tmap(_appended(!_isempty) ∘ _intersect ∘ first) |>
             _filter(last) |>
             _tmap(bisect ∘ first)
@@ -63,14 +69,6 @@ function _setup_search(with_contains_root, with_contraction_and_isinterior, with
             last.(remaining_region_tuples)
         )
     end
-end
-
-function setup_search(f, contractor, root_regions, indeterminate_regions, tolerance)
-    with_contains_root = _appended(strictly_contains_root(f))
-    with_contraction_and_isinterior =
-        _appended(strictly_interior) ∘ _appended(contractor)
-    with_too_small = _appended(too_small(tolerance) ∘ first)
-    return _setup_search(with_contains_root, with_contraction_and_isinterior, with_too_small, root_regions, indeterminate_regions, tolerance)
 end
 
 function _roots(
@@ -90,22 +88,21 @@ function _roots(
         indeterminate_regions,
         tolerance
     )
-    n = isnothing(num_tasks_hint) ? 4Threads.nthreads() :
+    n_tasks = isnothing(num_tasks_hint) ? 4Threads.nthreads() :
         max(num_tasks_hint, Threads.nthreads())
-    while length(regions) > 0
-        #@show length(regions)
-        #=
-        while length(regions) < n
-            split_region_tuples = bisect.(regions)
-            regions = vcat(
-                first.(split_region_tuples),
-                last.(split_region_tuples)
-            )
-        end
-        =#
+    while length(regions) < n_tasks
+        split_region_tuples = bisect.(regions)
         regions = vcat(
-            filter_contract_and_bisect(@view regions[1:min(n, length(regions))]),
-            @view regions[n + 1:end]
+            first.(split_region_tuples),
+            last.(split_region_tuples)
+        )
+    end
+    # TODO use fold() method here
+    while length(regions) > 0
+        @show length(regions)
+        regions = vcat(
+            filter_contract_and_bisect(Iterators.take(regions, n_tasks) |> collect),
+            @view regions[n_tasks + 1:end]
         )
     end
     return root_regions, indeterminate_regions
@@ -132,7 +129,6 @@ end
 # or we find a way to make sieve thread-safe
 
 # not clear how we can avoid the allocations – we want to be non mutating
-
 include("../examples/smiley_examples.jl")
 using .SmileyExample55
 
